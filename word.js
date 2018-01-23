00fd87942ec6aa1ef7407b47307347c101f48600
gbmahili <georgesmahili@gmail.com>
2018-01-23 07:37:42 -0500

    added function to ask the user to enter the letter 5 times for now

--- word.js
+++ word.js
@@ -9,31 +9,64 @@ var Word = function(dash){
     this.makeDashes = function(){
         // Create a random number
         var randNum = Math.floor(Math.random() * this.words.length) + 0;
-        var wordToGuess = this.words[randNum];
-        var dasheedWordArray = [];
+        // Select a word to guess randomly from the array
+        wordToGuess = this.words[randNum];
+        // Instanciate the word to guess
+        var dashedWordArray = [];
+        // Loop through the wordToGuess length and create dashe
         for (let i = 0; i < wordToGuess.length; i++) {
-            dasheedWordArray.push(this.dash);            
+            dashedWordArray.push(this.dash);            
         }
-        var dashedWord = "";
-        dasheedWordArray.forEach(element => {
-            dashedWord += element;
+        // Instanciate the dashedWord
+        dashedWord = "";
+        // Create the word in dashes
+        dashedWordArray.forEach(element => {
+            dashedWord += `${element}`;
         });
         
-        console.log(wordToGuess);
-        console.log(dashedWord);
+        //console.log(wordToGuess);
+        console.log(`\n${dashedWord.length}`);
     };
 };
 
-// inquirer.prompt([
-//     {
-//         type: "input",
-//         name: "letterTyped",
-//         message: "Enter a letter to start"
-//     }
-// ]).then(answers => {
-//     // Use user feedback for... whatever!!
-//     console.log(answers);
-// });
+var count = 0;
 
-var bob = new Word("_");
+var showLetter = function() {
+
+    if(count < 5) {
+
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "letterTyped",
+                message: "Guess a letter..."
+            }
+        ]).then(answers => {
+            var letterTyped = answers.letterTyped;
+            var indexesOfLetterTyped = [];
+            var wordToGuessArray = wordToGuess.split("");
+            wordToGuessArray.forEach((element, i) => {
+                if(element === letterTyped){
+                    indexesOfLetterTyped.push(i);
+                }
+            });
+            // console.log(indexesOfLetterTyped);
+            // console.log(wordToGuessArray);
+            String.prototype.replaceAt = function (index, replacement) {
+                return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+            }
+            indexesOfLetterTyped.forEach(element => {
+                dashedWord = dashedWord.replaceAt(element, letterTyped);        
+            });
+            console.log(dashedWord);
+
+            count++;
+            showLetter();
+        });
+    }
+};
+
+showLetter();
+
+var bob = new Word("-");
 bob.makeDashes();
